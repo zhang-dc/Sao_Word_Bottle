2f7fde14d5c2f68e7545a5e465ad927839cd7cb8
zhang-dc <1134128909@qq.com>
2019-01-24 21:22:28 +0800

    开始写主页

--- src/components/Index.1.vue
+++ src/components/Index.1.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="index-container">
-    asda
+    
   </div>
 </template>
 
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="index-container">
-    <div class="tobeach">
-
+    <div class="logo">
+      <img src="../assets/images/logo.png" alt="">
     </div>
     <div class="mybottles">
-
+       ← 我的瓶子
     </div>
-    asda
-    <div @click='asdasd(sadas)' class="aaa" v-show="ss">
-
-    </div>
-    <div v-for="item in dx" :key="item.s">
-      {{item.s}}
+    <div class="tobeach">
+      去海滩
+      <br>
+      ↓
     </div>
   </div>
 </template>
@@ -21,46 +19,67 @@ export default {
   name: 'Index',
   data () {
     return {
-      sadas: 1,
-      ss: false,
-      dx: [
-        {
-          s: 1,
-          b: 1,
-        },
-        {
-          s: 1,
-          b: 1,
-        },
-        {
-          s: 1,
-          b: 1,
-        }
-      ]
+
     }
   },
   methods: {
-    asdasd(a) {
-      if (a == 1) {
-        alert(a)
-        this.sadas = 0
-      } else if (a == 0) {
-        alert(a)
-        this.sadas = 1
-      }
-    }
+
   },
   created () {
-    alert('aaas')
-    
+
   }
 }
 </script>
 
 <style scoped>
-.aaa {
-  height: 50px;
-  width: 20px;
-  background: black;
+@font-face{
+  font-family: 'Seto';
+  src: url('../assets/fonts/Seto.ttf');
+  font-weight: normal;
+  font-style: normal;
+}
+.index-container {
+  width: 100%;
+  height: 100%;
+  background: url('../assets/images/主页背景.png') no-repeat;
+  background-size: 100% 100%;
+}
+.logo {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+}
+.logo img {
+  width: 70%;
+  height: auto;
+}
+.mybottles {
+  margin-left: 40%;
+  width: 60%;
+  height: 40%;
+  font-size: 2.6rem;
+  font-family: 'Seto';
+  font-weight: bold;
+  color: lightgreen;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.tobeach {
+  width: 100%;
+  height: 30%;
+  font-size: 2rem;
+  font-family: 'Seto';
+  color: aliceblue;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 </style>
